c4dfa5fbadcd4a8aec10d4c73aeb724cdf6c7c92
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2024-09-18 11:44:35 +0330

    table

--- src/pages/app/candidate/voteList/VoteList.tsx
+++ src/pages/app/candidate/voteList/VoteList.tsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { Table } from "react-bootstrap";
+import { FaCircleCheck } from "react-icons/fa6";
+import "./VoteList.scss";
+import img from "../../../../assets/download.jpg";
 
 const VoteList = () => {
-  return <div>VoteList</div>;
+  return (
+    <>
+      <table className="text-center mt-4 tbl">
+        <thead className="text-light ">
+          <tr>
+            <th></th>
+            <th colSpan={2}>نام کاندید</th>
+            <th>تعداد رای‌ها</th>
+            <th>درصد رای</th>
+            <th>رای شما</th>
+          </tr>
+        </thead>
+
+        <tbody className="tbl_danger">
+          <tr>
+            <td>
+              <div className="tbl_img">
+                <img src={img} />
+              </div>
+            </td>
+            <td colSpan={2}>ساراسادات کریمی</td>
+            <td>50</td>
+            <td>50</td>
+            <td>
+              <FaCircleCheck />
+            </td>
+          </tr>
+        </tbody>
+
+        <tbody className="tbl_blue">
+          <tr>
+            <td>
+              <div className="tbl_img">
+                <img src={img} />
+              </div>
+            </td>
+            <td colSpan={2}>ساراسادات کریمی</td>
+            <td>50</td>
+            <td>50</td>
+            <td>
+              <FaCircleCheck />
+            </td>
+          </tr>
+        </tbody>
+
+        <tbody className="tbl_accept">
+          <tr>
+            <td>
+              <div className="tbl_img">
+                <img src={img} />
+              </div>
+            </td>
+            <td colSpan={2}>ساراسادات کریمی</td>
+            <td>50</td>
+            <td>50</td>
+            <td>
+              <FaCircleCheck />
+            </td>
+          </tr>
+        </tbody>
+
+        <tbody>
+          <tr>
+            <td>
+              <div className="tbl_img">
+                <img src={img} />
+              </div>
+            </td>
+            <td colSpan={2}>ساراسادات کریمی</td>
+            <td>50</td>
+            <td>50</td>
+            <td>
+              <FaCircleCheck />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default VoteList;
