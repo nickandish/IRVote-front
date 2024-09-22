fb1b333c342b8113888d32510696ce637ef48663
Sarina-R <SarinaRezanezhadfard@gmail.com>
2024-09-22 22:28:49 +0330

    comment

--- src/pages/app/ticket/comment/Comment.tsx
+++ src/pages/app/ticket/comment/Comment.tsx
@@ -8,11 +8,11 @@ const Comment = () => {
   return (
     <>
       <Header title={"کامنت"} />
-      <Navbar />
-
       <div className="container">
-        <h2>انجام شده</h2>
         <Row className="box">
+          <Col className="col-12">
+            <h2>انجام شده</h2>
+          </Col>
           <Col className="col-12">
             <p>
               شماره تیکت : <span> T321QWE </span>
@@ -28,28 +28,51 @@ const Comment = () => {
               تیم مربوط : <span> فروش </span>
             </p>
           </Col>
+
+          <Row className="service">
+            <Col className="col-6">
+              <p>
+                نام سرویس : <span> - </span>
+              </p>
+            </Col>
+            <Col className="col-6">
+              <p>
+                تاریخ سرویس : <span> 03/07/01 </span>{" "}
+              </p>
+            </Col>
+          </Row>
         </Row>
-        <Row className="service">
-          <Col className="col-6">
-            <p>
-              نام سرویس : <span> - </span>
-            </p>
-          </Col>
-          <Col className="col-6">
-            <p>
-              تاریخ سرویس : <span> 03/07/01 </span>
-            </p>
-          </Col>
-        </Row>
+
         <div className="comment_box">
-          <Col className="col-12">
-            <p className=" p_1">سلااااامممممم</p>
-          </Col>
-          <Col className="col-12">
-            <p className=" p_2">سلااااامممممم</p>
-          </Col>
+          <div className="client">
+            <p>
+              سلااااااااامممممم سلاااااااممممم سلااااااممممممم سلاااااااممممم
+            </p>
+          </div>
+          <div className="help">
+            <div className="postibaani">
+              <p>
+                سلاااااااممممم سلااااااااامممممممممممم سلااااااامممممم
+                سلاااااامممممم سلاااااااممممم
+              </p>
+            </div>
+          </div>
+
+          <div className="client">
+            <p>
+              سلااااااااامممممم سلاااااااممممم سلااااااممممممم سلاااااااممممم
+            </p>
+          </div>
+          <div className="help">
+            <div className="postibaani">
+              <p>
+                سلاااااااممممم سلااااااامممممم سلاااااامممممم سلاااااااممممم
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+      <Navbar />
     </>
   );
 };
