cd40bc8ce8beacccb4d476f2f103eb455433e6a8
Sarina-R <Sarina.r.n.0@gmail.com>
2024-09-30 18:07:28 +0330

    main-link

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { HiOutlineUserGroup } from "react-icons/hi";
 import { MdHowToVote } from "react-icons/md";
 import Bg from "./login/Bg";
+import { Link } from "react-router-dom";
 import "./main.scss";
 
 const Main = () => {
@@ -10,14 +11,18 @@ const Main = () => {
       <Bg />
       <Container className="main-page">
         <Row className="holding">
-          <Col className="col-12 vote">
-            <button>میخواهم رای بدهم</button>
-            <MdHowToVote className="icon-1 icon" />
-          </Col>
-          <Col className="col-12  election">
-            <button>میخواهم انتخابات برگزار کنم</button>
-            <HiOutlineUserGroup className="icon-2 icon" />
-          </Col>
+          <Link to="/my-elections">
+            <Col className="col-12 vote">
+              <button>میخواهم رای بدهم</button>
+              <MdHowToVote className="icon-1 icon" />
+            </Col>
+          </Link>
+          <Link to="/ticketAdd">
+            <Col className="col-12  election">
+              <button>میخواهم انتخابات برگزار کنم</button>
+              <HiOutlineUserGroup className="icon-2 icon" />
+            </Col>
+          </Link>
         </Row>
       </Container>
     </>
