e25147327e9fad001d4f5d953bf3f8d16534c7e0
Sarina-R <Sarina.r.n.0@gmail.com>
2024-09-30 18:00:24 +0330

    main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { MdHowToVote } from "react-icons/md";
+import Bg from "./login/Bg";
+import "./main.scss";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <Bg />
+      <Container className="main-page">
+        <Row className="holding">
+          <Col className="col-12 vote">
+            <button>میخواهم رای بدهم</button>
+            <MdHowToVote className="icon-1 icon" />
+          </Col>
+          <Col className="col-12  election">
+            <button>میخواهم انتخابات برگزار کنم</button>
+            <HiOutlineUserGroup className="icon-2 icon" />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Main;
