839710c75f0bcf35eb6e0d26b11748cc678c5b5a
Sarina-R <Sarina.r.n.0@gmail.com>
2024-10-09 18:42:08 +0330

    img

--- src/pages/app/duration/Box.tsx
+++ src/pages/app/duration/Box.tsx
@@ -5,17 +5,18 @@ import { FaAngleLeft } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import "../../../scss/myElection.tsx/myElection.scss";
 
-interface BoxProps {
+interface DurationBoxProps {
   duration: {
     id: number;
     fa_title: string;
     start_at: string;
     end_at: string;
     status: number;
+    logo: string;
   };
 }
 
-const Box: React.FC<BoxProps> = ({ duration }) => {
+const DurationBox: React.FC<DurationBoxProps> = ({ duration }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -41,14 +42,32 @@ const Box: React.FC<BoxProps> = ({ duration }) => {
     duration.status
   );
 
+  // Construct the full image URL using Vite's environment variables
+  const imageUrl = `${import.meta.env.VITE_API_BASE_URL}${duration.logo}`;
+
   return (
-    <Col className="col-md-6 col-12 mb-3" onClick={handleClick}>
+    <Col
+      className="col-md-6 col-12 mb-3"
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    >
       <Row
         className={`fw-bold box d-flex justify-content-center ${statusClass}`}
       >
         <Row className="top d-flex pt-3">
           <Col className="col-1">
             <LiaBoxOpenSolid className="icon icon-box" />
+            {/* Display the logo image */}
+            {/* <img
+              src={imageUrl}
+              alt={`${duration.fa_title} logo`}
+              className="duration-logo"
+              loading="lazy"
+              onError={(e) => {
+                (e.target as HTMLImageElement).src =
+                  "/images/default-image.png"; // Ensure this path is correct
+              }}
+            /> */}
           </Col>
           <Col className="col-10">
             <h6 className="fw-bold pt-1">{duration.fa_title}</h6>
@@ -84,4 +103,4 @@ const Box: React.FC<BoxProps> = ({ duration }) => {
   );
 };
 
-export default Box;
+export default DurationBox;
