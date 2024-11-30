ebfc8c6eadf4da01535c238bf7fc7a1210b22420
Sarina-R <Sarina.r.n.0@gmail.com>
2024-11-30 23:45:38 +0330

    candidate panel desc --css

--- src/pages/app/panel/candidate/DescCandidate.tsx
+++ src/pages/app/panel/candidate/DescCandidate.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Card } from "react-bootstrap";
+import "./candidatePanel.scss";
 
 const DescCandidate = () => {
-  return <div>DescCandidate</div>;
+  return (
+    <Card className="mt-4 candidate-panel_desc">
+      <Card.Body>
+        <h5 className="desc-title mb-3">توضیحات کاندید درباره خود:</h5>
+        <p className="desc-text">
+          توضیحات کاندید درباره خود توضیحات کاندید درباره خود توضیحات کاندید
+          درباره خود توضیحات کاندید درباره خود توضیحات کاندید درباره خود توضیحات
+          کاندید درباره خود توضیحات کاندید درباره خود توضیحات کاندید درباره خود.
+        </p>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default DescCandidate;
