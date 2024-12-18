00f2a3d18b4570923c07b27c9d45d0842f87b0c7
Sarina-R <Sarina.r.n.0@gmail.com>
2024-12-18 15:29:26 +0330

    candidateResultChart --api

--- src/pages/app/panel/observer/Observer.tsx
+++ src/pages/app/panel/observer/Observer.tsx
@@ -7,8 +7,10 @@ import Chart from "./charts/Chart";
 import VoterLog from "./lists/voterLog/VoterLog";
 import CandidateLog from "./lists/candidateLog/CandidateLog";
 import BallotLog from "./lists/ballotLog/BallotLog";
-import "./observer.scss";
 import { DurationProvider } from "../../../../api/contextApi/DurationContext";
+import CandidateVoteResult from "./charts/charts/CandidateVoteResult";
+import DocVoterResult from "./charts/charts/DocVoterResult";
+import "./observer.scss";
 
 const Observer = () => {
   const navigate = useNavigate();
@@ -35,6 +37,16 @@ const Observer = () => {
       icon: <TiGroupOutline />,
       path: "/observerPanel/ballot-logs",
     },
+    {
+      title: "نمایش نتایج کاندیدها",
+      icon: <TiGroupOutline />,
+      path: "/observerPanel/candidate-result",
+    },
+    {
+      title: "نمایش نتایج سندها",
+      icon: <TiGroupOutline />,
+      path: "/observerPanel/document-result",
+    },
   ];
 
   return (
@@ -51,6 +63,8 @@ const Observer = () => {
           <Route path="/voter-logs" element={<VoterLog />} />
           <Route path="/candidate-logs" element={<CandidateLog />} />
           <Route path="/ballot-logs" element={<BallotLog />} />
+          <Route path="/candidate-result" element={<CandidateVoteResult />} />
+          <Route path="/document-result" element={<DocVoterResult />} />
         </Routes>
       </DurationProvider>
     </div>
--- src/pages/app/panel/observer/charts/charts/CandidateVoteResult.tsx
+++ src/pages/app/panel/observer/charts/charts/CandidateVoteResult.tsx
@@ -1,7 +1,116 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Cell,
+} from "recharts";
+import apiClient from "../../../../../../api/axios";
+import { API_URLS } from "../../../../../../api/urls";
+import { useDuration } from "../../../../../../api/contextApi/DurationContext";
+import Loading from "../../../../../../component/loading/Loading";
+
+interface CandidateResult {
+  candidate_id: number;
+  candidate_name: string;
+  weighted_votes: number;
+  percentage: number;
+}
+
+interface BallotData {
+  Main_count: number;
+  Reserve_count: number;
+  results: CandidateResult[];
+}
 
 const CandidateVoteResult = () => {
-  return <div>CandidateVoteResult</div>;
+  const [ballotData, setBallotData] = useState<any>(null);
+  const { observerDurationId } = useDuration();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiClient.get(
+          API_URLS.CANDIDATE_RESULT_CHART.replace(
+            ":id",
+            String(observerDurationId)
+          )
+        );
+        setBallotData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [observerDurationId]);
+
+  if (!ballotData) return <Loading />;
+
+  return (
+    <Container className="obs-list candidate-chart">
+      <h3 className="mt-5">فعالیت رای دهندگان</h3>
+      {Object.keys(ballotData).map((ballotName) => (
+        <div key={ballotName}>
+          <h4>{ballotName}</h4>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              width={600}
+              height={400}
+              data={transformChartData(ballotData[ballotName])}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="weighted_votes" name="تعداد آراء">
+                {transformChartData(ballotData[ballotName]).map(
+                  (entry, index) => (
+                    <Cell key={`cell-${index}`} className={entry.className} />
+                  )
+                )}
+              </Bar>
+              <Bar dataKey="percentage" name="درصد آراء">
+                {transformChartData(ballotData[ballotName]).map(
+                  (entry, index) => (
+                    <Cell key={`cell-${index}`} className={entry.className} />
+                  )
+                )}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      ))}
+    </Container>
+  );
 };
+
+function transformChartData(ballot: BallotData) {
+  const { results, Main_count, Reserve_count } = ballot;
+
+  return results.map((candidate: CandidateResult, index: number) => {
+    let className = "red-bar";
+    if (index < Main_count) {
+      className = "green-bar";
+    } else if (index < Main_count + Reserve_count) {
+      className = "blue-bar";
+    }
+
+    return {
+      name: candidate.candidate_name,
+      weighted_votes: candidate.weighted_votes,
+      percentage: candidate.percentage,
+      className,
+    };
+  });
+}
 
 export default CandidateVoteResult;
